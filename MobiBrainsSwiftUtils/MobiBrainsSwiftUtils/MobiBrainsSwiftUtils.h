36737e4b4df13eefaa4edd563c2baa66664401dd
Mahesh Asabe <mahesh.asabe@in-it0544.globant.com>
2018-05-17 14:16:03 +0530

    Initial framework setup

---
+++ MobiBrainsSwiftUtils/MobiBrainsSwiftUtils/MobiBrainsSwiftUtils.h
@@ -0,0 +1,19 @@
+//
+//  MobiBrainsSwiftUtils.h
+//  MobiBrainsSwiftUtils
+//
+//  Created by Mahesh Asabe on 17/05/18.
+//  Copyright © 2018 Mahesh Asabe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MobiBrainsSwiftUtils.
+FOUNDATION_EXPORT double MobiBrainsSwiftUtilsVersionNumber;
+
+//! Project version string for MobiBrainsSwiftUtils.
+FOUNDATION_EXPORT const unsigned char MobiBrainsSwiftUtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MobiBrainsSwiftUtils/PublicHeader.h>
+
+
---
+++ MobiBrainsSwiftUtils/MobiBrainsSwiftUtils/Test.swift
@@ -0,0 +1,16 @@
+//
+//  Test.swift
+//  MobiBrainsSwiftUtils
+//
+//  Created by Mahesh Asabe on 17/05/18.
+//  Copyright © 2018 Mahesh Asabe. All rights reserved.
+//
+
+import Foundation
+
+public class TestClass {
+    
+    public static func getFrameWorkName() -> String {
+        return "Framework name is MobiBrainsSwiftUtils"
+    }
+}
