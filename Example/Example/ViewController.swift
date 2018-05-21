3cd6137058690577b01de52da853ca4d28892a84
Mahesh Asabe <mahesh.asabe@in-it0544.globant.com>
2018-05-21 14:30:50 +0530

    Example updated

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -11,18 +11,56 @@ import MobiBrainsSwiftUtils
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var themeLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        printAppInfo()
+        validateEmail()
+        logging()
+        themeCheck()
+    }
+    
+    func printAppInfo() {
+        print(AppInfo.version)
+        print(AppInfo.name)
+        print(AppInfo.buildNumber)
+        print(AppInfo.deviceModel)
+        print(AppInfo.systemVersion)
+    }
+    
+    func validateEmail()  {
+    
+        let validator = Validator.init(fieldName: "Email", rules: [EmailValidator()])
         
-        print(TestClass.getFrameWorkName())
+        do {
+            try validator.validate("maheshgmail.com")
+        } catch let error as ValidationError {
+            print("\(error.localizedDescription)")
+        }catch {
+            print("unexpected error")
+        }
+    }
+    
+    func logging() {
+        
+        Log.logLevel = .verbose
+        
+        Log.info("Hello world")
+        Log.verbose("Ignore me")
+        Log.error("Don't ignore me!")
+    }
+    
+    func themeCheck() {
+        Theme.shared.setupTheme(withThemeFileName: "DefaultTheme")
+        
+        themeLabel.attributedText = NSAttributedString(string: "Hey, This is Mahesh", attributes: Theme.shared.attributesForLabel("headerLabel"))
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
